76aa477976db6606d50f5afedd2d313bddb47cba
dungdt23 <dungtiendao01@gmail.com>
2024-09-05 19:00:01 +0700

    Initial

--- DockerDemoWebApi/Migrations/20240904093422_Initial.Designer.cs
+++ DockerDemoWebApi/Migrations/20240904093422_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DockerDemoWebApi.Migrations
 {
     [DbContext(typeof(DockerComposeDemoDbContext))]
-    [Migration("20240419235045_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20240904093422_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
